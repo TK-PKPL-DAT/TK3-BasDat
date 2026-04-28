b923e1b4ee740be62257d86bf95f76d8b979fbc5
riyaan-bai <riyaanb2306@gmail.com>
2026-04-28 08:30:42 +0700

    making front end design

--- fitur_merah/urls.py
+++ fitur_merah/urls.py
@@ -4,5 +4,12 @@ from . import views
 app_name = 'fitur_merah'
 
 urlpatterns = [
-    # Nanti kita akan tambahkan path CRUD di sini
+    path('seats/', views.list_seat, name='list_seat'),
+    path('seats/tambah/', views.create_seat, name='create_seat'),
+    path('seats/edit/<uuid:id>/', views.update_seat, name='update_seat'),
+    path('seats/hapus/<uuid:id>/', views.delete_seat, name='delete_seat'),
+    path('my-tickets/', views.list_ticket, name='list_ticket'),
+    path('tickets/tambah/', views.create_ticket, name='create_ticket'),
+    path('tickets/edit/<uuid:id>/', views.update_ticket, name='update_ticket'),
+    path('tickets/hapus/<uuid:id>/', views.delete_ticket, name='delete_ticket'),
 ]
